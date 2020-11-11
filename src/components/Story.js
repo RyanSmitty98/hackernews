c7947b71327160af3ddc93a6954612540a4b3904
RyanSmitty98 <Ryan_Smith303@mymail.eku.edu>
2020-11-11 01:24:30 -0500

    Still have work to finish but am coming along

---
+++ src/components/Story.js
@@ -0,0 +1,21 @@
+import React, {useState, useEffect} from 'react';
+import {getStory} from  '../services/hnApi'
+import { StoryWrapper } from '../styles/StoryStyles'
+
+export const Story = ({storyId}) => {
+    const [story, setStory] = useState({});
+
+    useEffect(() => {
+        getStory(storyId).then(data => data && data.url && setStory(data));
+    }, []);
+
+    return story && story.url ? (
+        <StoryWrapper data-testid="story">
+            <a href={story.url}>
+                <p>{story.title}</p>
+            </a>
+            By: <p>{story.by}</p>
+            Posted: <p>{story.time}</p>
+        </StoryWrapper>
+    ): null;
+};
